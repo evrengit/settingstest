b2e9586bb067c5f2e38a2b56240e114dc6a6f4a8
Evren Pehlivan <evren_pehlivan@epam.com>
2025-06-16 18:21:01 +0300

    .

--- sample1/Controllers/WeatherForecastController.cs
+++ sample1/Controllers/WeatherForecastController.cs
@@ -12,28 +12,19 @@ namespace sample1.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-      
-
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger,IConfiguration configuration)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {  var config = _configuration.GetConnectionString("DefaultConnection");
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = config
-            })
-            .ToArray();
+        public string Get()
+        {
+            return _configuration.GetConnectionString("DefaultConnection").ToString();
         }
     }
 }
